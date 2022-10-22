6ea517170bdba318aa41fc193e1dd30f112f0fc9
ATixonov <tixxxonnnofff@rambler.ru>
2022-10-22 22:38:02 +0300

    Save & Load text on Buttons

--- app/src/main/java/ru/synergy/sharedpreferencesexample/MainActivity.java
+++ app/src/main/java/ru/synergy/sharedpreferencesexample/MainActivity.java
@@ -2,13 +2,60 @@ package ru.synergy.sharedpreferencesexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText et;
+    Button btnSave, btnLoad;
+    SharedPreferences sharedPreferences;
+    final String SAVED_TEXT = "saved text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.editText);
+        btnSave = (Button) findViewById(R.id.buttonsave);
+        btnLoad = (Button) findViewById(R.id.buttonload);
+
+        btnSave.setOnClickListener(this);
+        btnLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonsave:
+                saveText();
+                break;
+            case R.id.buttonload:
+                loadText();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void saveText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(SAVED_TEXT, et.getText().toString());
+        editor.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_LONG).show();
+    }
+
+    private void loadText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String savedText = sharedPreferences.getString(SAVED_TEXT, "Nothing to save");
+        et.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_LONG).show();
     }
 }
